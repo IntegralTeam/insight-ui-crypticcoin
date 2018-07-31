27b4b4a5229fd6ec570f966f6fdc560e31820219
stan <s.k.golovin@gmail.com>
2018-07-31 10:05:32 +0700

    ZEC -> CRYP

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var testnet = false;
-var netSymbol = testnet ? 'TAZ' : 'ZEC';
+var netSymbol = testnet ? 'TAC' : 'CRYP';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
 var defaultCurrency = localStorage.getItem('insight-currency') || netSymbol;
